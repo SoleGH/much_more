0474e918c6592dcbfccfcf5c4480134af9f2f753
yangbf <soleyang4g@gmail.com>
2018-11-02 18:58:57 +0800

    update flask tornado web app

--- python/flask/app/test/flask_tornado_app.py
+++ python/flask/app/test/flask_tornado_app.py
@@ -28,10 +28,34 @@ def main():
     http_server = HTTPServer(WSGIContainer(app))
     http_server.listen(5000)
 
+    # 开启多线程,但start需要调用os.fork()函数,windows下os没有该函数
     # http_server.bind(5000)
-    # http_server.start(4)
+    # http_server.start(0)
 
     IOLoop.instance().start()
+"""
+单进程:
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5000)
+IOLoop.instance().start()
+
++++++++++++++++++++++++++++++
+多进程
+http_server = HTTPServer(WSGIContainer(app))
+# 开启多线程,但start需要调用os.fork()函数,windows下os没有该函数
+# http_server.bind(5000)
+# http_server.start(0)  # 指定fork子进程数量
+
+IOLoop.instance().start()
+
++++++++++++++++++++++++++++++
+高级多进程
+sockets = tornado.netutil.bind_sockets(8888)
+tornado.process.fork_processes(0)
+server = HTTPServer(app)
+server.add_sockets(sockets)
+IOLoop.current().start()
+"""
 
 if __name__ == '__main__':
     main()
